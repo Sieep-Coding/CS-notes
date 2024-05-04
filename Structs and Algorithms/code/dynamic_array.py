074cd296a054079bb27811f27879f514b07cbcd5
Sieep-Coding <nstamb6@gmail.com>
2024-05-04 07:33:22 -0700

    dynamic arrays

--- Structs and Algorithms/code/dynamic_array.py
+++ Structs and Algorithms/code/dynamic_array.py
@@ -2,38 +2,46 @@ class DynamicArray:
     
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.size = 0
-        self.arr = [0] * capacity
+        self.length = 0
+        self.arr = [0] * self.capacity
 
+    # Get value at i-th index
     def get(self, i: int) -> int:
         return self.arr[i]
 
-
-    def insert(self, i: int, n: int) -> None:
+    # Set n at i-th index
+    def set(self, i: int, n: int) -> None:
         self.arr[i] = n
 
+    # Insert n in the last position of the array
     def pushback(self, n: int) -> None:
-        if self.size == self.capacity:
+        if self.length == self.capacity:
             self.resize()
+            
+        # insert at next empty position
+        self.arr[self.length] = n
+        self.length += 1
 
-        self.arr[self.size]
-        self.size += 1
-
+    # Remove the last element in the array
     def popback(self) -> int:
-        self.size -= 1
-        return self.arr[self.size]
+        if self.length > 0:
+            # soft delete the last element
+            self.length -= 1
+        # return the popped element
+        return self.arr[self.length]
 
     def resize(self) -> None:
+        # Create new array of double capacity
         self.capacity = 2 * self.capacity
-        new_arr = [0] * self.capacity
-
-        for i in range(self.size):
-            new_arr[i]=self.arr[i]
+        new_arr = [0] * self.capacity 
+        
+        # Copy elements to new_arr
+        for i in range(self.length):
+            new_arr[i] = self.arr[i]
         self.arr = new_arr
 
     def getSize(self) -> int:
-        return self.size
-        
+        return self.length
     
     def getCapacity(self) -> int:
         return self.capacity
